64254379fa083e77a46df5c2a1f3593c28ea7a7f
leejunhao <raymondlee755@gmail.com>
2024-10-17 16:59:54 +0800

    return all request header

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
-const http = require('https');
+const https = require('https');
 const url = require('url');
 const fs = require('fs');
-const port = 443;
+const port = 2052;
 const hostname = "0.0.0.0";
 
+// HTTPS server options with TLS configurations
 const httpsOptions = {
     cert: fs.readFileSync('/home/fae/Documents/CA_Cert/certificate.crt'),
     ca: fs.readFileSync('/home/fae/Documents/CA_Cert/ca_bundle.crt'),
-    key: fs.readFileSync('/home/fae/Documents/CA_Cert/private.key')
-
+    key: fs.readFileSync('/home/fae/Documents/CA_Cert/private.key'),
+    secureOptions: require('constants').SSL_OP_NO_SSLv2 |
+                   require('constants').SSL_OP_NO_SSLv3 |
+                   require('constants').SSL_OP_NO_TLSv1 |
+                   require('constants').SSL_OP_NO_TLSv1_1,
+    ciphers: 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384',
+    honorCipherOrder: true
 };
 
-
-const server = https.createServer(function (req, res) {
+const server = https.createServer(httpsOptions, function (req, res) {
     console.log(`Received request from ${req.connection.remoteAddress}: ${req.method} ${req.url}`);
     
+    // Parse the request URL
     var q = url.parse(req.url, true);
+    
+    // Check if the requested path is '/headers' to return HTTP request headers
+    if (q.pathname === '/headers') {
+        // Return all HTTP request headers as JSON in the response body
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(req.headers, null, 2));
+        return res.end();
+    }
+
+    // Serve static files for other paths
     var filename = "/home/fae/my-first-worker2/src/" + q.pathname;
     
     fs.readFile(filename, function(err, data) {
@@ -29,9 +45,9 @@ const server = https.createServer(function (req, res) {
         res.write(data);
         return res.end();
     });
-})
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+// Start the server and listen on the specified port and hostname
+server.listen(port, hostname, () => {
+  console.log(`Server running at https://${hostname}:${port}/`);
+});
